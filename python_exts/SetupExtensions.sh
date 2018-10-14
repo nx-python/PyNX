2c0be531117d50c2ea2d7d4876964ef2b29f201c
noah the goodra <peterpan0413@live.com>
2018-10-13 21:11:31 -0500

    allow recursive copying of the c files

--- python_exts/SetupExtensions.sh
+++ python_exts/SetupExtensions.sh
@@ -9,5 +9,5 @@ if [ $files != "0" ]; then
 fi
 files2=$(ls $TOPDIR/python_exts/c_files/* 2>/dev/null | wc -l)
 if [ $files2 != "0" ]; then
-	cp $TOPDIR/python_exts/c_files/* $TOPDIR/python_build/$PYDIR/Modules/
+	cp -r $TOPDIR/python_exts/c_files/* $TOPDIR/python_build/$PYDIR/Modules/
 fi
